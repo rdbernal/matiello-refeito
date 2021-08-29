bdf100d6892553ed122ef2c8cf2614d89420e6c7
Rodrigo Bernal <rodrigobernal19@gmail.com>
2021-08-29 19:35:47 -0300

    PHP dos formulários versão 1

--- email_simule.php
+++ email_simule.php
@@ -2,26 +2,30 @@
 
 if(isset($_POST['email']) && !empty($_POST['email'])) {
     
-    $nome = addslashes($_POST['nome']);
+    $nome = addslashes($_POST['name']);
     $email = addslashes($_POST['email']);
-    $contato = addslashes($_POST['telefone']);
-    
-    
-    
-    $to = "felixadvogadosad@gmail.com";
-    $subject = "Contato - Maurício Felix Advogados";
-    $body = "Nome: ".$nome."\r\n".
+    $contato = addslashes($_POST['telephone']);
+    $servico = addslashes($_POST['services']);
+    $parcelas = addslashes($_POST['parcelas'])
+    $valorParcelas = addslashes($_POST['valorParcelas'])
+    $message = addslashes($_POST['message']);
+
+
+    $to = "rodrigobernal19@gmail.com";
+    $subject = "Contato - Matiello Assessoria";
+    $body = "nome: ".$nome."\r\n".
             "E-mail: ".$email."\r\n".
-            "Celular: ".$contato;
-    $header = "From:contato@felixadvogados.adv.br"."\r\n".
+            "Contato: ".$contato."\r\n".
+            "Serviço: ".$servico."\r\n".
+            "Número de parcelas: ".$parcelas."\r\n".
+            "Valor das parcelas: ".$valorParcelas."\r\n".
+            "Mensagem: ".$message;
+
+    $header = "From: rodrigobernal19@gmail.com"."\r\n".
               "Reply-To:".$email."\r\n".
               "X=Mailer:PHP/".phpversion();
-              
-    
-    mail($to,$subject,$body,$header);
+    mail($to, $subject, $body, $header);
     header("Location: index.html?mailsend");
-
-    
 }
 
 
